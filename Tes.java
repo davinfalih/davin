a1b977eb3841caee728bbe8e3fc531c1b2259cfd
davinfalih <davin_ramadhan_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:15:50 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,8 @@
+public class Tes {
+    public static void main(String[] args) {
+    String nama = "Davin";
+    String kelas = "X RPL 5";
+    System.out.print("Halo, nama saya " + nama + " dari kelas " + kelas);
+}
+
+}
